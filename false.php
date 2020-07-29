22bdbd8bc3c210a78f06e302df64c739cdba24d6
kractus101 <67240838+kractus101@users.noreply.github.com>
2020-07-29 17:48:59 +0530

    Update false.php

--- false.php
+++ false.php
@@ -3,12 +3,50 @@ $fname= $_POST['fname'];
 $lname= $_POST['lname'];
 $email= $_POST['email'];
 
+if(isset($_POST['insert']))
+{
+    $file=$_FILES['file'];
+	$fileName= $_FILES['file']['name'];
+	$fileType= $_FILES['file']['type'];
+	$fileTmpName= $_FILES['file']['tmp_name'];
+	$fileError= $_FILES['file']['error'];
+	$fileSize= $_FILES['file']['size'];
+
+	$fileExt=explode('.',$fileName);//to seperate two pieces of data of filename
+	$fileActualExt=strtolower(end($fileExt));
+
+	$allowed= array('jpg','jpeg','png','pdf');
+	if(in_array($fileActualExt,$allowed))
+	{
+		if($fileError==0)
+		{
+			if($fileSize<10000000)
+			{
+				$fileNameNew=uniqid('',true).".".$fileActualExt;
+				$fileDestination='upload/'.$fileNameNew;
+				move_uploaded_file($fileTmpName,$fileDestination);
+				header("Location: js.php?uploadsuccess");
+			}
+			else{
+				echo "Your file is too big to upload";
+			}
+		}
+		else{
+			echo "There was an error uploading your file!";
+		}
+	}
+	else{
+		echo "You cannot upload files of this type";
+	}
+
+}
+
 if(!empty($fname) || !empty($lname) || !empty($email))
 {
     $host="localhost:3306";
     $dbusername="root";
     $dbpassword=" ";
-    $dbname="coursera";
+    $dbname="mysql";
 
     $conn = mysqli_connect($host,$dbusername,$dbpassword,$dbname);
     if(mysqli_connect_error())
@@ -16,8 +54,8 @@ if(!empty($fname) || !empty($lname) || !empty($email))
         die('Connect Error('. mysqli_connect_error().')'. mysqli_connect_error());  
     }
     else{
-        $SELECT = "SELECT email FROM register WHERE email=? Limit 1";
-        $INSERT = "INSERT Into register(fname,lname,email) values(?,?,?)";
+        $SELECT = "SELECT email FROM register1 WHERE email=? Limit 1";
+        $INSERT = "INSERT Into register1(fname,lname,email) values(?,?,?)";
         
         //prepare statement
         $stmt= $conn->prepare($SELECT);
@@ -58,11 +96,15 @@ if(!empty($fname) || !empty($lname) || !empty($email))
         }
         else{
             echo "Someone already registered using this email";
+            
+            }
+
+
         }
         $stmt->close();
         $conn->close();
     }
-}
+
 else
 {
     echo "all field are required";
